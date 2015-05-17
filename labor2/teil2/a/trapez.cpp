d14f23b4ca13ca9ff32b9995d26c704821800562
Philipp Meißner <pm@zeit.io>
2015-05-17 11:55:47 +0200

    added first layout for 2.a

---
+++ labor2/teil2/a/trapez.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double trapezFlaeche(float a, float c, float b, float winkel);
+
+
+int main () {
+  return 0;
+}
+
+double trapezFlaeche(float a, float c, float b, float winkel) {
+
+
+}
