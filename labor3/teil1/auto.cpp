c9190b9eea2f4d1a91c8a24827597905438abb23
Climax85 <climax85@web.de>
2015-05-26 09:28:44 +0200

    teil1 starteteil1 startedd

---
+++ labor3/teil1/auto.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Auto {
+	private:
+		int	 leistung;
+		char* fabrikat;
+	public:
+		static int anzahl;
+		Auto() {
+			anzahl++;
+			cout << "Anzahl: " << anzahl << endl;
+		}
+		
+		Auto(int leistung, char* fabrikat) {
+			Auto();
+			this->leistung = leistung;
+			this->fabrikat = fabrikat;
+		}
+		
+};
+
+int Auto::anzahl = 0;
+
+int main () {
+	for (int i = 0; i < 4; i++) {
+		Auto();
+	}
+}
