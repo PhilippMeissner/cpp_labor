36a2827b5d48f6e8339e1034626fb31ecee6cf01
Philipp Meißner <pm@zeit.io>
2015-05-29 13:24:08 +0200

    initiated first object

--- labor3/teil3/intmenge.cpp
+++ labor3/teil3/intmenge.cpp
@@ -1,11 +1,12 @@
 #include "intmenge.h"
-#include <iostream>
 using namespace std;
 
 // Public:
 IntMenge::IntMenge() {}
 
-void IntMenge::hinzufuegen(int el) {}
+void IntMenge::hinzufuegen(int el) {
+
+}
 void IntMenge::entfernen(int el) {}
 bool IntMenge::istMitglied(int el) {}
 int IntMenge::size() {}
--- labor3/teil3/main.cpp
+++ labor3/teil3/main.cpp
@@ -1,8 +1,9 @@
-#include "intmenge.cpp"
 #include <iostream>
+#include <vector>
+#include "intmenge.cpp"
 using namespace std;
 
 int main () {
-
+  IntMenge menge;
   return 0;
 }
