1a5db39b0ff66cf5b39ddd674ae45839153bbcd6
Philipp Meißner <pm@zeit.io>
2015-06-10 16:27:10 +0200

    first few functions..:poop::poop:

--- labor4/teil2/a/matrix.h
+++ labor4/teil2/a/matrix.h
@@ -15,9 +15,14 @@ class Zeile {
     int size;
 
   public:
-    // TODO: Konstruktor und Destruktor
-    Zeile( int s);
-    ~Zeile();
+    Zeile( int s) {
+      this->z = new int[s];
+      this->size = s;
+    };
+
+    ~Zeile() {
+      delete[] z;
+    }
 
     // Indexoperator [] ist (in zwei Versionen) bereits implementiert für die Klasse Zeile
     int& operator[](int i) {
@@ -36,12 +41,42 @@ class Matrix {
     Zeile **mat; // Zeiger auf "Zeilen"-Vektor
     int nrows, ncols; // Zeilen- und Spaltenzahl
 
-  public:
+  // public:
     // TODO: Zwei Konstruktoren, ein Kopierkonstruktor und ein Destruktor
-    Matrix( int _nrows , int _ncols);
-    Matrix( int z, int s, int wert);
-    Matrix( const Matrix& );
-    ~Matrix();
+    Matrix( int _nrows , int _ncols) {
+      mat = new Zeile*[_nrows];
+      for(int i = 0; i < _nrows; i++) {
+        *mat[i] = new Zeile*[_ncols];
+      }
+    };
+
+    Matrix( int z, int s, int wert) {
+      mat = new Zeile*[z];
+      for(int i = 0; i < z; i++) {
+        *mat[i] = new Zeile*[s];
+        for(int j = 0; j < s; j++) {
+          mat[i][j] = wert;
+        }
+      }
+    };
+
+    Matrix( const Matrix &m) {
+      mat = new Zeile*[m.nrows];
+      for(int i = 0; i < m.nrows; i++) {
+        *mat[i] = new Zeile*[m.ncols];
+        for(int j = 0; j < m.ncols; j++) {
+          this->mat[i][j] = m.mat[i][j];
+        }
+      }
+    };
+
+    ~Matrix() {
+      for(int i = 0; i < nrows; i++) {
+        for(int j = 0; j < ncols; j++) {
+          delete[] mat[i][j];
+        }
+      }
+    };
 
     // TODO: Zuweisungsoperator für tiefe Kopie (falls in Vorlesung besprochen)
     Matrix& operator=( const Matrix& ma );
