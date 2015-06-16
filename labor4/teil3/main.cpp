62e067bcdaeb5179533ce57cd380975c608a59ee
Philipp Meißner <pm@zeit.io>
2015-06-16 15:19:33 +0200

    added test-code for integers and doubles

--- labor4/teil3/main.cpp
+++ labor4/teil3/main.cpp
@@ -4,19 +4,35 @@ using namespace std;
 
 int main () {
 
-  Matrix<int> A(3,4);
+  // Testing double
+  Matrix<double> A(3,4);
   for(int i = 0; i < A.getCols(); i++){
-  	A[0][i] = i + 1;
-  	A[1][i] = i + 11;
-  	this->A[2][i] = i + 21;
+  	A[0][i] = (i + 1.00)/10.00;
+  	A[1][i] = (i + 11.00)/10.00;
+  	A[2][i] = (i + 21.00)/10.00;
   }
 
   // Kopie von A nach B
-  Matrix<int> B(A);
+  Matrix<double> B(A);
   // Addition der beiden Matrizen
   cout << (A + B) << endl << endl;
   // Multiplikation von A * B'
   cout << A * B.transpose() << endl << endl;
+
+  // Testing int
+  Matrix<int> C(3,4);
+  for(int i = 0; i < C.getCols(); i++){
+  	C[0][i] = i + 1.00;
+  	C[1][i] = i + 11.00;
+  	C[2][i] = i + 21.00;
+  }
+
+  // Kopie von C nach D
+  Matrix<int> D(C);
+  // Addition der beiden Matrizen
+  cout << (C + D) << endl << endl;
+  // Multiplikation von C * D'
+  cout << C * D.transpose() << endl << endl;
 
   return 0;
 }
