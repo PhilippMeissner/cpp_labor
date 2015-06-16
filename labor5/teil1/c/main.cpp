624e2a5ade9c65f31f544c2d799b37db2e9a26cd
Philipp Meißner <pm@zeit.io>
2015-06-16 17:41:05 +0200

    added first implementations

--- labor5/teil1/c/main.cpp
+++ labor5/teil1/c/main.cpp
@@ -1,57 +1,7 @@
 #include <iostream>
-#include "vector.cpp"
+#include "punkt.h"
 using namespace std;
 
 int main() {
-  // Testing int
-  cout << "Integer Template Test:" << endl;
-  Vector<int> v1;
-  cout << "v1: " << v1 << endl;
-
-  Vector<int> v2(8);
-  cout << "v2: " << v2 << endl;
-  cout << "Minimum von v2: " << v2.min() << endl;
-
-  Vector<int> v3(v2);
-  cout << "v3: " << v3 << endl;
-  cout << "Anzahl von v3: " << v3.getAnzahl() << endl;
-
-  if ( kleiner(v3[2], v2[5]) ) cout << v3[2] << " ist kleiner als " << v2[5] << endl;
-
-  int arr[5] = { 10, 5, 2, 3, 12 };
-
-  Vector<int> v4;
-  cout << "v4: " << v4 << endl;
-  v4.setVector( arr, 4 );
-  cout << "v4 nach set: " << v4 << endl;
-  cout << "Minimum von v4: " << v4.min() << endl;
-  cout << "Anzahl von v4: " << v4.getAnzahl() << endl;
-
-
-
-  // Testing double
-  cout <<  endl << endl << "Double Template Test:" << endl;
-    Vector<double> v5;
-  cout << "v5: " << v5 << endl;
-
-  Vector<double> v6(8);
-  cout << "v6: " << v6 << endl;
-  cout << "Minimum von v6: " << v6.min() << endl;
-
-  Vector<double> v7(v6);
-  cout << "v7: " << v7 << endl;
-  cout << "Anzahl von v7: " << v7.getAnzahl() << endl;
-
-  if ( kleiner(v7[2], v6[5]) ) cout << v7[2] << " ist kleiner als " << v6[5] << endl;
-
-  double darr[5] = { 10.0 / 1.00, 5.0/ 1.00, 2.0 / 1.00, 3.0 / 1.00, 12.0 / 1.00 };
-
-  Vector<double> v8;
-  cout << "v8: " << v8 << endl;
-  v8.setVector( darr, 4 );
-  cout << "v8 nach set: " << v8 << endl;
-  cout << "Minimum von v8: " << v8.min() << endl;
-  cout << "Anzahl von v8: " << v8.getAnzahl() << endl;
-
   return 0;
 }
---
+++ labor5/teil1/c/punkt.h
@@ -0,0 +1,37 @@
+// Datei: punkt.h
+#ifndef _PUNKT_H_
+#define _PUNKT_H_
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Punkt {
+  private:
+    float x,y;
+
+  public:
+    Punkt( float x0 = 0, float y0 = 0 );
+    Punkt ( const Punkt& p ); // Kopierkonstruktor
+    ~Punkt();
+    void setX( float x0 );
+    void setY( float y0);
+    float getX() const;
+    float getY() const;
+    float abstand00() const;
+    float abstand( const Punkt& p ) const;
+    friend ostream& operator<< ( ostream& os, const Punkt& p );
+};
+
+inline Punkt::Punkt( float x0, float y0 ) : // TODO
+inline Punkt::Punkt( const Punkt& p ) : // TODO
+inline Punkt::~Punkt() {}
+inline void Punkt::setX( float x0 ) { this->x = x0; }
+inline void Punkt::setY( float y0 ) { this->y = y0; }
+inline float Punkt::getX() const { return this->x; }
+inline float Punkt::getY() const { return this->y; }
+inline float Punkt::abstand00() const { return sqrt( x*x + y*y ); }
+inline float Punkt::abstand( const Punkt&p ) const { return sqrt( (x-p.x)*(x-p.x) + (y-p.y)*(y-p.y) ); }
+inline ostream& operator<< ( ostream& os, const Punkt& p ) { os << "(" << p.x << ", " << p.y << ")"; return os; }
+
+#endif
