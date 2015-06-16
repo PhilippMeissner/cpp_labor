15c6003d549331c7276672490f06bbf628175cb3
Philipp Meißner <pm@zeit.io>
2015-06-16 16:49:54 +0200

    copied functions

--- labor5/teil1/b/vector.cpp
+++ labor5/teil1/b/vector.cpp
@@ -1,63 +1,62 @@
 // Datei: vector.cpp
-
 #include "vector.h"
 
-Vector::Vector( int a ) : anzahl(a)
-{
+// Überladener Konstruktor
+Vector::Vector( int a ) : anzahl(a) {
 	v = new int[a];
-	for ( int i = 0; i < a; i++ )
-	v[i] = i;
+	for ( int i = 0; i < a; i++ ) {
+    v[i] = i;
+  }
 }
 
-Vector::Vector( const Vector& vec )
-{
-	…
+// Kopierkonstruktor
+Vector::Vector( const Vector& vec ) {
+  this->anzahl = vec.anzahl;
+  this->v = new int[vec.anzahl];
+  for (int i = 0; i < vec.anzahl; i++) {
+    this->v[i] = vec.v[i];
+  }
 }
 
-Vector::~Vector()
-{
-	…
+// Destruktor
+Vector::~Vector() {
+  delete[] v;
 }
 
-bool kleiner( const int& a, const int& b )
-{
+bool kleiner( const int& a, const int& b ) {
 	return ( a < b );
 }
 
-int Vector::min() const
-{
+int Vector::min() const {
 	int min = v[0];
-	for ( int i = 1; i < anzahl; i++ )
-	{
-	if ( v[i] < min )
-	min = v[i];
+	for ( int i = 1; i < anzahl; i++ ) {
+    if ( v[i] < min ) min = v[i];
 	}
 	return min;
 }
 
-int Vector::getAnzahl() const
-{
-	…;
+int Vector::getAnzahl() const {
+  return this->anzahl;
 }
 
-int Vector::operator[] ( const int i ) const
-{
-	…;
+// Überladener Index-Operator
+int Vector::operator[] ( const int i ) const {
+  return this->v[i];
 }
 
-void Vector::setVector( int* sv, int sanzahl )
-{
+void Vector::setVector( int* sv, int sanzahl ) {
 	delete[] v; // alten Inhalt loeschen
 	anzahl = sanzahl;
 	v = new int[anzahl];
-	for ( int i = 0; i < anzahl; i++ )
-	v[i] = sv[i];
+	for ( int i = 0; i < anzahl; i++ ) {
+    v[i] = sv[i];
+  }
 	return;
 }
 
-ostream& operator<< ( ostream& os, const Vector& v )
-{
-	for ( int i = 0; i < v.anzahl; i++ )
-	os << v[i] << ", ";
+ostream& operator<< ( ostream& os, const Vector& v ) {
+	for ( int i = 0; i < v.anzahl; i++ ) {
+    os << v[i] << ", ";
+  }
 	return os;
 }
