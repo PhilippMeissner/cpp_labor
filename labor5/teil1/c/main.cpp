5964f17259993e12099b10a0c59862b645b97ff3
Philipp Meißner <pm@zeit.io>
2015-06-21 22:04:44 +0200

    changed datatype to 'Punkt'

--- labor5/teil1/c/main.cpp
+++ labor5/teil1/c/main.cpp
@@ -4,24 +4,24 @@
 using namespace std;
 
 int main() {
-  // Testing double
-  cout <<  endl << endl << "double Template Test:" << endl;
-  Vector<double> v5;
+  // Testing Punkt
+  cout <<  endl << endl << "Punkt Template Test:" << endl;
+  Vector<Punkt> v5;
   cout << "v5: " << v5 << endl;
 
-  Vector<double> v6(8);
+  Vector<Punkt> v6(8);
   cout << "v6: " << v6 << endl;
   cout << "Minimum von v6: " << v6.min() << endl;
 
-  Vector<double> v7(v6);
+  Vector<Punkt> v7(v6);
   cout << "v7: " << v7 << endl;
   cout << "Anzahl von v7: " << v7.getAnzahl() << endl;
 
   if ( kleiner(v7[2], v6[5]) ) cout << v7[2] << " ist kleiner als " << v6[5] << endl;
 
-  double darr[5] = { 10.0 / 1.00, 5.0/ 1.00, 2.0 / 1.00, 3.0 / 1.00, 12.0 / 1.00 };
+  Punkt darr[5] = { 10.0 / 1.00, 5.0/ 1.00, 2.0 / 1.00, 3.0 / 1.00, 12.0 / 1.00 };
 
-  Vector<double> v8;
+  Vector<Punkt> v8;
   cout << "v8: " << v8 << endl;
   v8.setVector( darr, 4 );
   cout << "v8 nach set: " << v8 << endl;
